55c926b089d3fe25a6fcd49c3644ba27f0289bd8
Shennoter <36178491+Shennoter@users.noreply.github.com>
2022-03-05 21:18:43 +0800

    First upload

---
+++ src/main/kotlin/ApexResponseMap.kt
@@ -0,0 +1,147 @@
+package pers.shennoter
+
+data class ApexResponseMap(
+    val arenas: Arenas,
+    val arenasRanked: ArenasRanked,
+    val battle_royale: BattleRoyale,
+    val control: Control,
+    val ranked: Ranked
+)
+
+data class Arenas(
+    val current: Current,
+    val next: Next
+)
+
+data class ArenasRanked(
+    val current: CurrentX,
+    val next: NextX
+)
+
+data class BattleRoyale(
+    val current: CurrentXX,
+    val next: NextXX
+)
+
+data class Control(
+    val current: CurrentXXX,
+    val next: NextXXX
+)
+
+data class Ranked(
+    val current: CurrentXXXX,
+    val next: NextXXXX
+)
+
+data class Current(
+    val DurationInMinutes: Int,
+    val DurationInSecs: Int,
+    val asset: String,
+    val code: String,
+    val end: Int,
+    val map: String,
+    val readableDate_end: String,
+    val readableDate_start: String,
+    val remainingMins: Int,
+    val remainingSecs: Int,
+    val remainingTimer: String,
+    val start: Int
+)
+
+data class Next(
+    val DurationInMinutes: Int,
+    val DurationInSecs: Int,
+    val code: String,
+    val end: Int,
+    val map: String,
+    val readableDate_end: String,
+    val readableDate_start: String,
+    val start: Int
+)
+
+data class CurrentX(
+    val DurationInMinutes: Int,
+    val DurationInSecs: Int,
+    val asset: String,
+    val code: String,
+    val end: Int,
+    val map: String,
+    val readableDate_end: String,
+    val readableDate_start: String,
+    val remainingMins: Int,
+    val remainingSecs: Int,
+    val remainingTimer: String,
+    val start: Int
+)
+
+data class NextX(
+    val DurationInMinutes: Int,
+    val DurationInSecs: Int,
+    val code: String,
+    val end: Int,
+    val map: String,
+    val readableDate_end: String,
+    val readableDate_start: String,
+    val start: Int
+)
+
+data class CurrentXX(
+    val DurationInMinutes: Int,
+    val DurationInSecs: Int,
+    val asset: String,
+    val code: String,
+    val end: Int,
+    val map: String,
+    val readableDate_end: String,
+    val readableDate_start: String,
+    val remainingMins: Int,
+    val remainingSecs: Int,
+    val remainingTimer: String,
+    val start: Int
+)
+
+data class NextXX(
+    val DurationInMinutes: Int,
+    val DurationInSecs: Int,
+    val code: String,
+    val end: Int,
+    val map: String,
+    val readableDate_end: String,
+    val readableDate_start: String,
+    val start: Int
+)
+
+data class CurrentXXX(
+    val DurationInMinutes: Int,
+    val DurationInSecs: Int,
+    val asset: String,
+    val code: String,
+    val end: Int,
+    val map: String,
+    val readableDate_end: String,
+    val readableDate_start: String,
+    val remainingMins: Int,
+    val remainingSecs: Int,
+    val remainingTimer: String,
+    val start: Int
+)
+
+data class NextXXX(
+    val DurationInMinutes: Int,
+    val DurationInSecs: Int,
+    val code: String,
+    val end: Int,
+    val map: String,
+    val readableDate_end: String,
+    val readableDate_start: String,
+    val start: Int
+)
+
+data class CurrentXXXX(
+    val asset: String,
+    val map: String
+)
+
+data class NextXXXX(
+    val map: String
+)
---
+++ src/test/kotlin/RunTerminal.kt
@@ -0,0 +1,28 @@
+package pers.shennoter
+
+import java.io.File
+import net.mamoe.mirai.alsoLogin
+import net.mamoe.mirai.console.MiraiConsole
+import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.enable
+import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.load
+import net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader
+
+fun setupWorkingDir() {
+    // see: net.mamoe.mirai.console.terminal.MiraiConsoleImplementationTerminal
+    System.setProperty("user.dir", File("debug-sandbox").absolutePath)
+}
+
+suspend fun main() {
+    setupWorkingDir()
+
+    MiraiConsoleTerminalLoader.startAsDaemon()
+
+    val pluginInstance = RankLookUp
+
+    pluginInstance.load() // 主动加载插件, Console 会调用 Ranklookup.onLoad
+    pluginInstance.enable() // 主动启用插件, Console 会调用 Ranklookup.onEnable
+
+    val bot = MiraiConsole.addBot(2829755835, "Wangjiaxin753951").alsoLogin() // 登录一个测试环境的 Bot
+
+    MiraiConsole.job.join()
+}
