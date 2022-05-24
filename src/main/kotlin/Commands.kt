8de69edd8d3e6d5d6e0dba4dee506c932e8148bb
Shennoter <36178491+Shennoter@users.noreply.github.com>
2022-05-24 21:26:08 +0800

    v1.4.0-pre update

--- src/main/kotlin/Commands.kt
+++ src/main/kotlin/Commands.kt
@@ -1,7 +1,16 @@
+import com.google.gson.Gson
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import net.mamoe.mirai.console.command.CommandSender
+import net.mamoe.mirai.console.command.CompositeCommand
 import net.mamoe.mirai.console.command.SimpleCommand
 import net.mamoe.mirai.contact.Contact.Companion.sendImage
 import pers.shennoter.*
+import pers.shennoter.RankLookUp.dataFolder
+import pers.shennoter.RankLookUp.logger
+import java.io.File
+import java.net.URL
+import java.util.*
 
 object Player : SimpleCommand(
     RankLookUp, "apexid","玩家查询" ,
@@ -146,8 +155,101 @@ object Cache : SimpleCommand(
 ) {
     @Handler
     suspend fun CommandSender.apexDelCache() {
-        var result = removeFileByTime(false)
+        val result = removeFileByTime(false)
         RankLookUp.logger.info(result)
         subject?.sendMessage(result)
+    }
+}
+
+object Listener : CompositeCommand(
+    RankLookUp, "apexadd", description = "添加监听",
+) {
+    @SubCommand
+    suspend fun CommandSender.id(id: String) {
+        val url = "https://api.mozambiquehe.re/bridge?version=5&platform=PC&player=$id&auth=${Config.ApiKey}"
+        try {
+            URL(url).readText()
+        }
+        catch(e:Exception){
+            subject?.sendMessage("玩家ID不存在")
+            logger.error("玩家ID不存在")
+        }
+        val gson = Gson()
+        val listendPlayer : ListendPlayer = gson.fromJson(File("$dataFolder/Data.json").readText(), ListendPlayer::class.java)
+        if(listendPlayer.data.contains(id)) {
+            if(listendPlayer.data[id]?.contains(subject?.id) == true) {
+                subject?.sendMessage("该玩家已在监听列表")
+                return
+            }
+            else {
+                listendPlayer.data[id]?.add(subject?.id)
+            }
+        }
+        else {
+            listendPlayer.data[id] = arrayListOf(subject?.id)
+        }
+        File("$dataFolder/Data.json").writeText(gson.toJson(listendPlayer))
+        logger.info("添加对${id}:${subject?.id}的监听成功")
+        subject?.sendMessage("添加对${id}:${subject?.id}的监听成功")
+        playerJob?.cancel()
+        playerJob = playerStatListener()
+    }
+
+    @SubCommand
+    suspend fun CommandSender.map() {
+        val gson = Gson()
+        val groups : GroupReminding = gson.fromJson(File("${RankLookUp.dataFolder}/Reminder.json").readText(), GroupReminding::class.java)
+        if(groups.data.contains(subject?.id)){
+            subject?.sendMessage("该群已在提醒列表")
+            logger.error("该群已在提醒列表")
+            return
+        }else {
+            groups.data.add(subject?.id)
+        }
+        File("$dataFolder/Reminder.json").writeText(gson.toJson(groups))
+        logger.info("已添加对${subject?.id}的地图轮换提醒")
+        subject?.sendMessage("已添加对${subject?.id}的地图轮换提醒")
+        mapTask?.cancel()
+        mapTask = mapReminder()
+    }
+}
+
+object ListenerRemove : CompositeCommand(
+    RankLookUp, "apexremove","移除id" ,
+    description = "取消对某玩家的监听"
+) {
+    @SubCommand
+    suspend fun CommandSender.id(id: String) {
+        val gson = Gson()
+        val listendPlayer : ListendPlayer = gson.fromJson(File("$dataFolder/Data.json").readText(), ListendPlayer::class.java)
+        if(listendPlayer.data.contains(id) && listendPlayer.data[id]?.contains(subject?.id) == true) {
+            listendPlayer.data[id]?.remove(subject?.id)
+            subject?.sendMessage("已取消对${id}:${subject?.id}的监听")
+            logger.info("已取消对${id}:${subject?.id}的监听")
+            playerJob?.cancel()
+            playerJob = playerStatListener()
+        }
+        else {
+            subject?.sendMessage("${id}:${subject?.id}不在监听列表")
+            logger.error("${id}:${subject?.id}不在监听列表")
+        }
+        File("$dataFolder/Data.json").writeText(gson.toJson(listendPlayer))
+    }
+
+    @SubCommand
+    suspend fun CommandSender.map(){
+        val gson = Gson()
+        val groups : GroupReminding = gson.fromJson(File("${RankLookUp.dataFolder}/Reminder.json").readText(), GroupReminding::class.java)
+        if(groups.data.contains(subject?.id)){
+            groups.data.remove(subject?.id)
+            subject?.sendMessage("已取消该群提醒")
+            logger.error("已取消该群提醒")
+            mapTask?.cancel()
+            mapTask = mapReminder()
+        }
+        else{
+            subject?.sendMessage("该群不在提醒列表")
+            logger.error("该群不在提醒列表")
+        }
     }
 }
--- src/main/kotlin/RankLookUp.kt
+++ src/main/kotlin/RankLookUp.kt
@@ -1,22 +1,34 @@
 package pers.shennoter
 
+
 import Cache
 import Craft
+import Listener
+import ListenerRemove
 import News
 import Player
 import Predator
 import Map
+import com.google.gson.Gson
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import net.mamoe.mirai.console.command.*
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import java.io.File
+import java.util.TimerTask
 
+var mapTask:TimerTask? = null
+var playerJob: Job? = null
 object RankLookUp : KotlinPlugin(
     JvmPluginDescription(
         id = "pers.shennoter.RankLookUp",
-        name = "RankLookUp",
-        version = "1.3.0",
-    )
+        name = "ApexLookUp",
+        version = "1.4.0",
+    ){
+        author("Shennoter")
+    }
 ){
     override fun onEnable() {
         logger.info("Apex查询插件已启动")
@@ -27,16 +39,59 @@ object RankLookUp : KotlinPlugin(
         CommandManager.registerCommand(Predator)
         CommandManager.registerCommand(News)
         CommandManager.registerCommand(Cache)
+        CommandManager.registerCommand(Listener)
+        CommandManager.registerCommand(ListenerRemove)
         logger.info("Apex查询插件已载入")
-        val folder1 = File("./data/pers.shennoter.ranklookup/")
+        val folder1 = File("./data/pers.shennoter.RankLookUp/")
         if(!folder1.exists()) {
             folder1.mkdirs()
         }
+        val folder2 = File("$dataFolder/score/")
+        if(!folder2.exists()) {
+            folder2.mkdirs()
+        }
+        val folder3 = File("$dataFolder/imgs/")
+        if(!folder3.exists()) {
+            folder3.mkdirs()
+        }
+        val listendID = File("$dataFolder/Data.json")
+        if (!listendID.exists()) {
+            File("$dataFolder/Data.json").writeText("{\"data\":{\"0\":[0]}}")
+        }
+        val remindingGroups = File("${RankLookUp.dataFolder}/Reminder.json")
+        if (!remindingGroups.exists()) {
+            File("$dataFolder/Reminder.json").writeText("{\"data\":[0]}")
+        }
         if(Config.ApiKey == ""){
-            logger.error("未找到ApiKey，请到 https://apexlegendsapi.com/ 获取ApiKey填入 ./config/pers.shennoter.ranklookup/config.yml 中并重启mirai-console")
+            logger.error("未找到ApiKey，请到 https://apexlegendsapi.com/ 获取ApiKey填入 ./config/pers.shennoter.RankLookUp/config.yml 中并重启mirai-console")
         }
         if(Config.cacheAutoDel == "true"){
             RankLookUp.logger.info(removeFileByTime(true))
         }
+        GlobalScope.launch {
+            var listendPlayer : ListendPlayer = Gson().fromJson(File("$dataFolder/Data.json").readText(), ListendPlayer::class.java)
+            if(Config.listener && listendPlayer.data.size > 1) {
+                playerJob = playerStatListener()
+            }
+            val groups : GroupReminding = Gson().fromJson(File("${RankLookUp.dataFolder}/Reminder.json").readText(), GroupReminding::class.java)
+            if(Config.mapRotationReminder && groups.data.size > 1){
+                mapTask = mapReminder()
+            }
+        }
+    }
+
+    override fun onDisable(){
+        Config.save()
+        CommandManager.unregisterCommand(Player)
+        CommandManager.unregisterCommand(Map)
+        CommandManager.unregisterCommand(Craft)
+        CommandManager.unregisterCommand(Predator)
+        CommandManager.unregisterCommand(News)
+        CommandManager.unregisterCommand(Cache)
+        CommandManager.unregisterCommand(Listener)
+        CommandManager.unregisterCommand(ListenerRemove)
+        logger.info("Apex查询插件已卸载")
     }
 }
+
+
