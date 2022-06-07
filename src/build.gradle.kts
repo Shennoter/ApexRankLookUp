dff2dd341351bf0556380634b5b33e62ba1112d7
Shennoter <36178491+Shennoter@users.noreply.github.com>
2022-06-07 14:16:53 +0800

    v1.5.1 update

---
+++ src/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    val kotlinVersion = "1.5.10"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
+
+    id("net.mamoe.mirai-console") version "2.10.0"
+}
+
+group = "pers.shennoter"
+version = "1.5.1"
+
+repositories {
+    maven("https://maven.aliyun.com/repository/public")
+    mavenCentral()
+    google()
+}
+
+dependencies {
+    implementation("com.google.code.gson:gson:2.9.0")
+}
